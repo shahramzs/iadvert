5c84638b185048888975f14022529ebb0cfeebea
shahramzs <55354494+shahramzs@users.noreply.github.com>
2021-07-19 16:13:28 +0430

    Add files via upload

---
+++ public/precache-manifest.7c6853406ee10d8c3e45bb75d7d4ec85.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47cf9afdee6bf026fed74dc3e9c1a281",
+    "url": "advertt.freevar.com/index.html"
+  },
+  {
+    "revision": "2b042d290d9d6dfec870",
+    "url": "advertt.freevar.com/static/css/2.155915ff.chunk.css"
+  },
+  {
+    "revision": "01accce86de3e09e4140",
+    "url": "advertt.freevar.com/static/css/main.045f7b5c.chunk.css"
+  },
+  {
+    "revision": "2b042d290d9d6dfec870",
+    "url": "advertt.freevar.com/static/js/2.0d7965f8.chunk.js"
+  },
+  {
+    "revision": "7724a5cd23ef66213c07362c977d7987",
+    "url": "advertt.freevar.com/static/js/2.0d7965f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01accce86de3e09e4140",
+    "url": "advertt.freevar.com/static/js/main.110056c1.chunk.js"
+  },
+  {
+    "revision": "4ddbe5b8eb0680229e86",
+    "url": "advertt.freevar.com/static/js/runtime-main.f95ac731.js"
+  },
+  {
+    "revision": "a494abb8c7ab68f1102d19b8391d5ec2",
+    "url": "advertt.freevar.com/static/media/1.a494abb8.svg"
+  },
+  {
+    "revision": "b6997e33572073ccb52064ed7abd3ce3",
+    "url": "advertt.freevar.com/static/media/1.b6997e33.mp4"
+  },
+  {
+    "revision": "196fd84f22af3c08863f74a818227843",
+    "url": "advertt.freevar.com/static/media/123.196fd84f.jpg"
+  },
+  {
+    "revision": "e4d78eb5cf7af1009fe05f8d6dd36f42",
+    "url": "advertt.freevar.com/static/media/2.e4d78eb5.svg"
+  },
+  {
+    "revision": "c8c6d1f1af1a0936ea347c6fcae9d68b",
+    "url": "advertt.freevar.com/static/media/3.c8c6d1f1.svg"
+  },
+  {
+    "revision": "9dbdb8f6dcf2f6b935e17a4bfcacd7c0",
+    "url": "advertt.freevar.com/static/media/4.9dbdb8f6.svg"
+  },
+  {
+    "revision": "cd1b69d6192d09bebc06717c0d587f72",
+    "url": "advertt.freevar.com/static/media/456.cd1b69d6.jpg"
+  },
+  {
+    "revision": "17cdfd7b19ff3094147b83039958e4fc",
+    "url": "advertt.freevar.com/static/media/5.17cdfd7b.svg"
+  },
+  {
+    "revision": "ca2c7961b289c41cae19efd9695e4f8d",
+    "url": "advertt.freevar.com/static/media/6.ca2c7961.svg"
+  },
+  {
+    "revision": "e374fe63634320507457f2174e070f8c",
+    "url": "advertt.freevar.com/static/media/789.e374fe63.jpg"
+  },
+  {
+    "revision": "fb7810a162bf25d3929a3e9e0b8120cd",
+    "url": "advertt.freevar.com/static/media/CassandraPersonalUseRegular-3BjG.fb7810a1.ttf"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "advertt.freevar.com/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "441ea48a4026881b19b653a83ccd3b6a",
+    "url": "advertt.freevar.com/static/media/a.441ea48a.jpg"
+  },
+  {
+    "revision": "a7e8be48013b04ceb3167797842ac12e",
+    "url": "advertt.freevar.com/static/media/advertUser.a7e8be48.png"
+  },
+  {
+    "revision": "fce52ffbaf1a6459d93558492a184d8e",
+    "url": "advertt.freevar.com/static/media/business.fce52ffb.svg"
+  },
+  {
+    "revision": "8a8b917c0093f1626079b8a28c530893",
+    "url": "advertt.freevar.com/static/media/chat.8a8b917c.mp3"
+  },
+  {
+    "revision": "10f74ea38bf19d192cb72cd1dd7155a0",
+    "url": "advertt.freevar.com/static/media/contact.10f74ea3.jpg"
+  },
+  {
+    "revision": "085fe95dcc27802cb3a816396e16238b",
+    "url": "advertt.freevar.com/static/media/customer.085fe95d.svg"
+  },
+  {
+    "revision": "d40e312528e3b63195d8c3a309ec4931",
+    "url": "advertt.freevar.com/static/media/discount.d40e3125.svg"
+  },
+  {
+    "revision": "6639defd87ec04baa1a44ac26b607625",
+    "url": "advertt.freevar.com/static/media/freeAdvert.6639defd.jpg"
+  },
+  {
+    "revision": "6496e71bd0ea5dd26e55a0f5d7db8482",
+    "url": "advertt.freevar.com/static/media/image-help.6496e71b.png"
+  },
+  {
+    "revision": "3e591290928d73c8b1f28236b4fa0eb2",
+    "url": "advertt.freevar.com/static/media/image.3e591290.png"
+  },
+  {
+    "revision": "8eb7c865a7b73e2157a14d96eeaaf13e",
+    "url": "advertt.freevar.com/static/media/loader1.8eb7c865.svg"
+  },
+  {
+    "revision": "2328a454ac4e9ff29e25a08312340376",
+    "url": "advertt.freevar.com/static/media/mac.2328a454.jpg"
+  },
+  {
+    "revision": "91c75b287ea4c9a16292221669bc903f",
+    "url": "advertt.freevar.com/static/media/per.91c75b28.jpg"
+  },
+  {
+    "revision": "9b49904b2bd1253b035f9a15a81690ca",
+    "url": "advertt.freevar.com/static/media/res.9b49904b.jpg"
+  }
+]);
